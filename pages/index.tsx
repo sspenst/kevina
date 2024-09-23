3e24ae95371db8761978fa22b806b250e720c880
Spencer Spenst <spencerspenst@gmail.com>
2024-09-22 21:55:47 -0700

    links

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,15 +19,20 @@ export default function Index() {
         <title>Kevina & Spencer Spenst</title>
         <meta name='description' content='Kevina & Spencer Spenst' />
       </Head>
-      <div className='m-16'>
-        <div className={classNames('flex flex-col gap-8 text-center text-4xl italic', libre.className)}>
-          <span>Kevina Grace Takenaka</span>
-          <span>&</span>
-          <span>Spencer Christian Spenst</span>
+      <div className='flex flex-col items-center gap-12 m-16'>
+        <div className={classNames('flex flex-col items-center gap-4 text-center text-3xl italic', libre.className)}>
+          <div className='flex flex-col gap-8 text-center text-4xl'>
+            <span>Kevina Grace Takenaka</span>
+            <span>&</span>
+            <span>Spencer Christian Spenst</span>
+          </div>
           <br />
           <span>November 15th, 2024</span>
           <span>4:00 pm in the afternoon</span>
-          <span>Fort Langley Community Hall</span>
+          <br />
+          <a className='hover:text-green-600 underline w-fit' href='https://maps.app.goo.gl/gSqQzDLs1HB7dX6C6' target='_blank' rel='noreferrer'>Fort Langley Community Hall</a>
+          <br />
+          <a className='hover:text-green-600 underline w-fit' href='https://docs.google.com/forms/d/e/1FAIpQLSfngKKVGGtjyomtK5lgZ-L7e8LF5WIhF258w7OGS_c3f-r6QQ/viewform' target='_blank' rel='noreferrer'>RSVP</a>
         </div>
       </div>
     </>
